75442f2b1cb35993ff90ffde9a90266eb709ab8c
Vahen <chris.xelo.pico@gmail.com>
2021-03-17 18:01:29 +0100

    [Web] - Ajout de la Vue JS pour l'app - Ajout d'un template de base - Corrections du code html dans la page index - Ajout du fichier css

---
+++ web/static/scripts/app.js
@@ -0,0 +1,31 @@
+// N'utiliser qu'une seul vue par fichier, éviter les Vue multiple
+const base_app_vue = new Vue({
+    el: '#app',
+    data: {
+        data_size: 20,
+        real_data: [],
+        interpolated_data: [],
+        difference_data: [],
+    },
+    methods: {
+        fetch_last_n_elements_to_display() {
+            let self = this;
+            let get_url = ""
+            axios.get(get_url,
+                {
+                    params:
+                        {size: this.data_size}
+                }
+            ).then(function (response) {
+                self.real_data = response.data.real_data;
+                self.interpolated_data = response.data.interpolated_data;
+                self.difference_data = response.data.difference_data;
+                console.log('SUCCESS!!');
+            }).catch(function (response) {
+                console.error(JSON.stringify(response.message));
+                console.log('FAILURE!!');
+            });
+        },
+    },
+    computed: {}
+});
