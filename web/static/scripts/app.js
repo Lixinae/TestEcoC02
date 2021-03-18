548020586db79d6708178edb43c64d208b86f3c6
Vahen <chris.xelo.pico@gmail.com>
2021-03-18 22:48:35 +0100

    [Web] - Correction de l'affichage - Ajout d'un input pour afficher le nombre de données - Ajout d'un watcher pour l'input pour afficher le tableau dynamiquement - Renommage plus explicite de fonctions

--- web/static/scripts/app.js
+++ web/static/scripts/app.js
@@ -2,21 +2,32 @@
 const app_vue = new Vue({
     el: '#app',
     data: {
-        co2_rate_all_data : []
+        co2_rate_all_data: [],
+        co2_rate_all_data_last_n: [],
+        numb_values_to_display: 20,
     },
     methods: {
-        fetch_last_n_elements_to_display() {
+        fetch_elements() {
             let self = this;
             let get_url = "api/LastElementsData/"
-            axios.get(get_url,{}
+            axios.get(get_url, {}
             ).then(function (response) {
                 self.co2_rate_all_data = response.data;
+                self.co2_rate_all_data_last_n = self.co2_rate_all_data.slice(-self.numb_values_to_display);
                 console.log('SUCCESS!!');
             }).catch(function (response) {
                 console.error(JSON.stringify(response.message));
                 console.log('FAILURE!!');
             });
         },
+    },
+    watch: {
+        numb_values_to_display: function(val){
+            if (val > 0){
+                this.co2_rate_all_data_last_n = this.co2_rate_all_data.slice(-val);
+            }
+
+        }
     },
     delimiters: ["<%", "%>"]
 });
