b536edd273e6f0709a25ac37fa9796864c73f24e
Vahen <chris.xelo.pico@gmail.com>
2021-03-18 16:41:15 +0100

    [Data_fetcher] - Correction du code pour correspondre aux changements des données en entrée - Suppression du code obsolète

--- app/data/data_fetcher.py
+++ app/data/data_fetcher.py
@@ -1,4 +1,3 @@
-# from Django_test.models import C02
 import datetime
 import json
 from typing import List
@@ -23,15 +22,7 @@ def grab_data_from_api(start_time, end_time):
 
     response = requests.get(url, headers=headers, params=payload)
     json_data = response.json()
-    # Pour chaque valeur json dans le tableau renvoyé
-    # Créer une entrée dans la BDD
-
     return json_data
-
-
-# def fetch_all_data_from_db():
-#     all_co2_data = C02.objects.all()
-#     return all_co2_data
 
 
 def filter_by_hour(value: json):
@@ -52,13 +43,13 @@ def convert_date_time_to_timestamp(date_time: str):
 
 
 def interpolate_data(current_data, previous_data, next_data):
-    current_data_time = convert_date_time_to_timestamp(current_data["datetime"])  # X
-    current_data_co2_rate = current_data["co2_rate"]  # On cherche à calculer cette valeur, Y
+    current_data_time = current_data["datetime"]  # X
+    # current_data_co2_rate = current_data["co2_rate"]  # On cherche à calculer cette valeur, Y
 
-    previous_time = convert_date_time_to_timestamp(previous_data["datetime"])  # X1
+    previous_time = previous_data["datetime"]  # X1
     previous_co2_rate = previous_data["co2_rate"]  # Y1
 
-    next_time = convert_date_time_to_timestamp(next_data["datetime"])  # X2
+    next_time = next_data["datetime"]  # X2
     next_co2_rate = next_data["co2_rate"]  # Y2
     # y = y1 + ((x – x1) / (x2 - x1) × (y2 - y1))
     co2_rate = previous_co2_rate + (
@@ -72,7 +63,7 @@ def interpolate_all_data_from_tmp(interpolated_data_tmp):
     interpolated_data = []
     index = 0
     for data_tmp in interpolated_data_tmp:
-        if data_tmp["co2_rate"] == -1:
+        if data_tmp["co2_rate"] == -1 and index+1 < len(interpolated_data_tmp):
             previous_data = interpolated_data_tmp[index - 1]
             next_data = interpolated_data_tmp[index + 1]
             data_interpolated = {
@@ -90,15 +81,14 @@ def generate_tmp_interpolated_data(filtered_data):
     interpolated_data_tmp = []
     i = 0
     for data in filtered_data:
-        interpolated_data_tmp.append(data)
+        data_json = data.to_json()
+        interpolated_data_tmp.append(data_json)
         # On ne peut interpoler que si l'on a la donnée précédente et suivante
         # Donc le dernier élément ne doit pas être ajouté
-        if i + 1 < len(filtered_data):
-            date_splits = data["datetime"].split(":")
-            date_splits[1] = "30"
-            date_time = ":".join(date_splits)
+        if i < len(filtered_data):
+            # On a une liste par heure et on veut rajouter les 30min entre chaque -> +1800
             new_data_to_interpolate = {
-                'datetime': date_time,
+                'datetime': data_json["datetime"] + 1800,
                 'co2_rate': -1
             }
             interpolated_data_tmp.append(new_data_to_interpolate)
@@ -111,14 +101,3 @@ def interpolate_data_aux(filtered_data):
     interpolated_data_tmp = generate_tmp_interpolated_data(filtered_data)
     interpolated_data = interpolate_all_data_from_tmp(interpolated_data_tmp)
     return interpolated_data
-
-
-# Todo -> Tout ce code sera dans une api à terme
-
-co2_data_fetched = grab_data_from_api("2017-01-01T00:00:00", "2019-01-01T00:00:00")
-filtered_data = filter_co2_data_to_one_hour_frequence(co2_data_fetched)
-interpolated_data = interpolate_data_aux(filtered_data)
-data_difference = [{
-    'datetime': x["datetime"],
-    'difference_abs': abs(x["co2_rate"] - y["co2_rate"])
-} for x, y in zip(co2_data_fetched, interpolated_data)]
