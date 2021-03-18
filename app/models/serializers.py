5666d853abc5460fcc4fd73b773483563b1dee3c
Vahen <chris.xelo.pico@gmail.com>
2021-03-18 16:39:55 +0100

    [models] - suppression du dossier

--- app/models/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from models import RealDataC02, FilteredDataC02
-
-
-class RealDataC02Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = RealDataC02
-        fields = ('date', 'co2_rate')
