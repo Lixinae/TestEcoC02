21f78df54f1f9fde582ff56ec7ad2e1c03f4059d
Vahen <chris.xelo.pico@gmail.com>
2021-03-19 02:30:05 +0100

    [Api] - Ajout de documentation [Urls] - Renommage pour correspondre au nouveau nom de l'api view

--- app/api/api_views.py
+++ app/api/api_views.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt
+from typing import List, Dict
 
 from django.http import JsonResponse
 
@@ -10,10 +11,17 @@ from app.data.data_fetcher import interpolate_data_aux
 from app.models import RealDataC02, FilteredDataC02
 
 
-class LastElementsData(APIView):
+class Co2RateData(APIView):
 
     @staticmethod
-    def calcul_moyenne_jours_ouvre_weekend(real_data_json_tmp, interpolated_data_json_tmp):
+    def calcul_moyenne_jours_ouvre_weekend(real_data_json_tmp:List[Dict], interpolated_data_json_tmp:List[Dict]):
+        """
+        Génère une liste de données de la même taille que celles en entrée
+        Cette liste contient les moyennes de C02 à l'instant T pour les jours ouvrés et week end
+        :param real_data_json_tmp: Les données réelle sur le C02
+        :param interpolated_data_json_tmp: Les données interpolé sur le C02
+        :return: Une liste de json avec les informations sur les moyennes de co2 pour les jours ouvrés et les weekend
+        """
         moy_json = []
         nb_data_jours_ouvres = 0
         nb_data_jours_weekend = 0
@@ -55,6 +63,22 @@ class LastElementsData(APIView):
         return moy_json
 
     def get(self, request):
+        """
+        Répond à la requête Get de l'api
+        Les champs ont des noms volontairment court pour réduire la taille des données envoyés
+        :param request: Contient les données de la requête get envoyé à l'api
+        :return: Une reponse JSON avec comme contenu une list de json au format
+        {
+            'dt': # Datetime
+            'r':  # Donnée C02 réel
+            'i':  # Donnée C02 interpolé
+            'dif': # Difference entre la donnée réelle et la donnée interpolé
+            'm_jo_r': # Moyenne à l'instant T des données C02 réel pour les jours ouvrés
+            'm_we_r': # Moyenne à l'instant T des données C02 réel pour les week end
+            'm_jo_i': # Moyenne à l'instant T des données C02 interpolé pour les jours ouvrés
+            'm_we_i': # Moyenne à l'instant T des données C02 interpolé pour les week end
+        }
+        """
         try:
             real_data_tmp = RealDataC02.objects.all()
             filtered_data = FilteredDataC02.objects.all()
@@ -71,19 +95,8 @@ class LastElementsData(APIView):
             # Les 4 list json doivent être de la même taille ici
             for i in range(len(real_data_json_tmp)):
                 if interpolated_data_json_tmp[i]["co2_rate"] != -1:
-                    # data = {
-                    #     'datetime': dt.fromtimestamp(real_data_json_tmp[i]["datetime"]).strftime("%Y-%m-%dT%H:%M:%S"),
-                    #     'real_co2_rate': real_data_json_tmp[i]["co2_rate"],
-                    #     'interpolated_co2_rate': interpolated_data_json_tmp[i]["co2_rate"],
-                    #     'difference': difference_data_json[i]["difference"],
-                    #     'moy_jours_ouvre_real_co2_rate': moy_json[i]["moy_jours_ouvre_real_co2_rate"],
-                    #     'moy_weekend_real_co2_rate': moy_json[i]["moy_weekend_real_co2_rate"],
-                    #     'moy_jours_ouvre_interpolated_co2_rate': moy_json[i]["moy_jours_ouvre_interpolated_co2_rate"],
-                    #     'moy_weekend_interpolated_co2_rate': moy_json[i]["moy_weekend_interpolated_co2_rate"],
-                    # }
                     data = {
                         'dt': dt.fromtimestamp(real_data_json_tmp[i]["datetime"]).strftime("%Y-%m-%dT%H:%M:%S"),
-                        # 'dt': real_data_json_tmp[i]["datetime"],
                         'r': real_data_json_tmp[i]["co2_rate"],
                         'i': interpolated_data_json_tmp[i]["co2_rate"],
                         'dif': difference_data_json[i]["difference"],
