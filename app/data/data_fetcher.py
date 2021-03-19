f40f7e36a449d5b65603d278eac8a0d6b35a9efd
Vahen <chris.xelo.pico@gmail.com>
2021-03-19 02:31:03 +0100

    [Data] - Ajout de documentation - Ajout de typage explicite pour plus de securité

--- app/data/data_fetcher.py
+++ app/data/data_fetcher.py
@@ -1,15 +1,16 @@
 import datetime
 import json
-from typing import List
 import requests
+from typing import List, Dict
+from app.models import FilteredDataC02
 
 
-def grab_data_from_api(start_time, end_time):
+def grab_data_from_api(start_time: str, end_time: str) -> List[Dict]:
     """
     Récupère la data à partir de l'api d'ecoco2
-    "http://api-recrutement.ecoco2.com/v1/data/?start_time="
-    :param start_time: Date de début des données
-    :param end_time: Date de fin des données
+    "http://api-recrutement.ecoco2.com/v1/data/"
+    :param start_time: Date de début des données au format "2017-01-01T00:00:00"
+    :param end_time: Date de fin des donnéesau format "2017-01-01T00:00:00"
     """
     url = "http://api-recrutement.ecoco2.com/v1/data/"
     payload = {
@@ -19,30 +20,52 @@ def grab_data_from_api(start_time, end_time):
     headers = {
         'Content-Type': 'application/json'
     }
-
     response = requests.get(url, headers=headers, params=payload)
     json_data = response.json()
     return json_data
 
 
-def filter_by_hour(value: json):
+def filter_by_hour(value: json) -> bool:
+    """
+    Fonction utilisé dans le filtre
+    Vérifie le 2e paramètre du split sur le champ "datetime" et indique si c'est un "00" (heure pleine)
+    :param value: Le json sur lequel on veut tester la valeur
+    :return: True si c'est l'on a une heure pleine
+    """
     date_splits = value["datetime"].split(":")
     # On sais que le 2e argument sera les minutes, 0 min -> heure pleine
     return date_splits[1] == "00"
 
 
-def filter_co2_data_to_one_hour_frequence(all_co2_data: List):
+def filter_co2_data_to_one_hour_frequence(all_co2_data: List[Dict]) -> List[Dict]:
+    """
+    Génère une nouvelle liste avec une fréquence par heure à partir des données en entrée
+    :param all_co2_data: Toute les données Co2 en entrée avec une fréquence de 30min
+    :return: Liste de données avec une fréquence de 30min
+    """
     filtered_data = list(filter(filter_by_hour, all_co2_data))
     return filtered_data
 
 
-def convert_date_time_to_timestamp(date_time: str):
+def convert_date_time_to_timestamp(date_time: str) -> float:
+    """
+    Convertis une date du format '%Y-%m-%dT%H:%M:%S' (Année - Mois - Jour T Heures:Minutes:Secondes) vers un timestamp
+    :param date_time: La date au format '%Y-%m-%dT%H:%M:%S'
+    :return: Le timestamp de la date en entrée
+    """
     date_time_obj = datetime.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
     # On doit rester en UTC pour éviter le bug avec le DST par rapport à la timezone
     return date_time_obj.replace(tzinfo=datetime.timezone.utc).timestamp()
 
 
-def interpolate_data(current_data, previous_data, next_data):
+def interpolate_data(current_data: Dict, previous_data: Dict, next_data: Dict) -> float:
+    """
+    Interpole la donnée current_data_co2_rate à partir des données précédente et suivante
+    :param current_data: La donnée actuelle
+    :param previous_data: La donnée précédente
+    :param next_data: La donnée suivante
+    :return: Le co2_rate interpolé à partir des données en entrée
+    """
     current_data_time = current_data["datetime"]  # X
     # current_data_co2_rate = current_data["co2_rate"]  # On cherche à calculer cette valeur, Y
 
@@ -59,11 +82,16 @@ def interpolate_data(current_data, previous_data, next_data):
     return co2_rate
 
 
-def interpolate_all_data_from_tmp(interpolated_data_tmp):
+def interpolate_all_data_from_tmp(interpolated_data_tmp: List[Dict]) -> List[Dict]:
+    """
+    Calcule toute les données à interpolé à partir de la liste de donnée en entrée
+    :param interpolated_data_tmp: Le tableau de données non interpolé, ayant pour valeur -1 s'il y a besoin d'interpolé
+    :return: Les données interpolé
+    """
     interpolated_data = []
     index = 0
     for data_tmp in interpolated_data_tmp:
-        if data_tmp["co2_rate"] == -1 and index+1 < len(interpolated_data_tmp):
+        if data_tmp["co2_rate"] == -1 and index + 1 < len(interpolated_data_tmp):
             previous_data = interpolated_data_tmp[index - 1]
             next_data = interpolated_data_tmp[index + 1]
             data_interpolated = {
@@ -77,7 +105,13 @@ def interpolate_all_data_from_tmp(interpolated_data_tmp):
     return interpolated_data
 
 
-def generate_tmp_interpolated_data(filtered_data):
+def generate_tmp_interpolated_data(filtered_data: List[FilteredDataC02]) -> List[Dict]:
+    """
+    Génère une liste de données à partir des données filtré
+    Indique -1 si la donnée doit être interpolé par la suite
+    :param filtered_data: Les données filtré par heure
+    :return: La liste de données complète, avec les données à interpolé
+    """
     interpolated_data_tmp = []
     i = 0
     for data in filtered_data:
@@ -97,7 +131,13 @@ def generate_tmp_interpolated_data(filtered_data):
 
 
 # Données en entré filtré par heure
-def interpolate_data_aux(filtered_data):
+def interpolate_data_aux(filtered_data: List[FilteredDataC02]) -> List[Dict]:
+    """
+    Génère la liste de données à interpolé
+    Réalise l'interpolation sur les éléments nécessaire et renvoie la liste des données interpolé
+    :param filtered_data: Les données filtré par heure
+    :return: La liste des données interpolé
+    """
     interpolated_data_tmp = generate_tmp_interpolated_data(filtered_data)
     interpolated_data = interpolate_all_data_from_tmp(interpolated_data_tmp)
     return interpolated_data
